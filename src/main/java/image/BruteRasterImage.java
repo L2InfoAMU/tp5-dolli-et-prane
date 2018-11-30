7f00cbfaa9cc62dc22628b70f5ea7e33ac2ed2f9
s16014348 <fsl12@otmail.fr>
2018-11-30 13:43:04 +0100

    Creation des Setter et des Getter pour les attributs ; With; Height; Pixels; Color;

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,11 +1,25 @@
 package image;
 
 import javafx.scene.paint.Color;
+import static util.Matrices.*;
 
 public class BruteRasterImage implements Image{
+
     private int width;
     private int height;
     private Color[][] pixels;
+    private Color color;
+
+    public BruteRasterImage(Color color, int width, int height){
+        this.color = color;
+        this.width = width;
+        this.height= height;
+    }
+
+    public BruteRasterImage(Color[][] colors){
+        requiresNonNull(colors);
+        this.pixels = colors;
+    }
 
 
     public Color getPixelColor(int x, int y){
@@ -18,6 +32,23 @@ public class BruteRasterImage implements Image{
 
     public int getHeight(){
         return this.height;
+    }
+
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setPixels(Color[][] pixels) {
+        this.pixels = pixels;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
 }
